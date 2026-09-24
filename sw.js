b2c2cb52ee2fd91ea604eedae3e64c53ec1be19d
中本　叡 <143879371+america-bowl@users.noreply.github.com>
2026-09-24 21:23:08 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,43 @@
+const CACHE_NAME = 'america-bowl-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './quiz-data.js',
+  './site.webmanifest',
+  './favicon.ico',
+  './apple-touch-icon.png'
+];
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
